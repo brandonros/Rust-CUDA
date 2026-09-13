ece6dbd40d2e63a994287808b31001cfe1a1a50b
Brandon Ros <brandonros1@gmail.com>
2026-09-13 13:58:29 -0400

    Fix PTX export target selection for LLVM 19

--- examples/ptx_export/src/main.rs
+++ examples/ptx_export/src/main.rs
@@ -1,4 +1,4 @@
-use cuda_builder::{CudaBuilder, NvvmArch};
+use cuda_builder::CudaBuilder;
 use std::{env, fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,7 +11,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let output = output.canonicalize()?;
     let kernels = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("kernels");
     let ptx = CudaBuilder::new(kernels)
-        .arch(NvvmArch::Compute89)
         .copy_to(output.join("rust_kernels.ptx"))
         .final_module_path(output.join("final-module.ll"))
         .emit_llvm_ir(true)
